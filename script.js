a099d6e536878b05c52cf8d36d707080650fe8e2
huekoh <huexkoh@gmail.com>
2025-07-01 14:05:20 +0800

    Create playRound function to determine winner

--- script.js
+++ script.js
@@ -11,6 +11,35 @@ function getHumanChoice() {
     return choice;
 }
 
+function playRound(h, c) {
+    let output = "";
+
+    let tempH = h[0].charCodeAt(0);
+    let tempC = c[0].charCodeAt(0);
+
+    console.log(tempH-tempC);
+
+    switch (tempH - tempC) {
+        case 0:
+            output = "it's a tie";
+            break;
+        case -2:
+        case -1:
+        case 3:
+            output = `you have won! you chose ${h} and your opponent chose ${c}.`;
+            break;
+        default:
+            output = `you have lost :(! you chose ${h} and your opponent chose ${c}.`;
+            break;
+    }
+
+    return output
+}
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+
 
